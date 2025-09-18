e8d9a2cd75f9648e25d17822b90a39622409444a
Nina E Schulz <ninaschulz_16@hotmail.com>
2025-09-18 13:51:13 +0100

    still going

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-let takoyakiCount = 0;
-let tps = 1;
+let takoyakiCount = 0
+let tps = 1
+
+let state = {
+    takoyakiCount: 0,
+    tps: 1
+}
+
+setInterval(function(){
+    takoyakiCount= takoyakiCount + tps
+    console.log (takoyakiCount)
+},1000)
+
