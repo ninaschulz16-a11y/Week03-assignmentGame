ee57378a7c96c5f231249360432b7c8d190a17f7
Nina E Schulz <ninaschulz_16@hotmail.com>
2025-09-18 17:05:00 +0100

    we are getting there !

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-let takoyakiCount = 0
-let tps = 1
 
 let state = {
     takoyakiCount: 0,
     tps: 1
+};
+
+function updateDisplay (){
+    takoyakiCountDisplay.textContent = state.takoyakiCount;
+    tpsDisplay.textContent = state.tps;
 }
 
 setInterval(function(){
-    takoyakiCount= takoyakiCount + tps
-    console.log (takoyakiCount)
+    state.takoyakiCount= state.takoyakiCount + state.tps
+    updateDisplay();
 },1000)
+
+const takoyaki = document.getElementById("takoyaki");
+const takoyakiCountDisplay = document.getElementById("takoyakiUPby");
+const tpsDisplay = document.getElementById("tps")
+
+
+takoyaki.addEventListener("click", ()=> {
+    state.takoyakiCount++;
+    updateDisplay();
+
+});
+updateDisplay();
+
+//Let's get that API now!
 
