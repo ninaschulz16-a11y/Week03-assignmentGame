239e314cd73cd5ec197a26bcf6a9907d5bfaf66a
Nina E Schulz <ninaschulz_16@hotmail.com>
2025-09-19 15:33:18 +0100

    TakoGameOn!

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+console.log("hello takoyaki lovers!");
+
 
 let state = {
     takoyakiCount: 0,
     tps: 1
 };
 
+function sum(a, b){
+    return a+b;
+}
+
+const takoyaki = document.getElementById("takoyaki");
+const takoyakiCountDisplay = document.getElementById("takoyakiUPby");
+const tpsDisplay = document.getElementById("tps")
+
 function updateDisplay (){
-    takoyakiCountDisplay.textContent = state.takoyakiCount;
-    tpsDisplay.textContent = state.tps;
+    takoyakiCountDisplay.textContent = "UP and UP: " + state.takoyakiCount;
+    tpsDisplay.textContent = "Takoyaki per second: " + state.tps;
 }
 
 setInterval(function(){
@@ -14,9 +24,6 @@ setInterval(function(){
     updateDisplay();
 },1000)
 
-const takoyaki = document.getElementById("takoyaki");
-const takoyakiCountDisplay = document.getElementById("takoyakiUPby");
-const tpsDisplay = document.getElementById("tps")
 
 
 takoyaki.addEventListener("click", ()=> {
@@ -27,4 +34,52 @@ takoyaki.addEventListener("click", ()=> {
 updateDisplay();
 
 //Let's get that API now!
+async function fetchData() {
 
+    const response = await fetch(`https://cookie-upgrade-api.vercel.app/api/upgrades`)
+   
+   const data = await response.json ();
+   
+   console.log(data);
+
+   const stall = document.getElementById("stall");
+
+   data.forEach(upgrade => {
+
+    const item = document.createElement("div");
+
+    item.classList.add("upgrade");
+
+    item.innerHTML =`
+      <h3>${upgrade.name}</h3>
+      <p>Cost: ${upgrade.cost}</p>
+      <p>+${upgrade.increase} TPS</p>
+    `;
+   
+    const  button = document.createElement("button");
+    button.innerText = "Buy";
+
+    button.addEventListener("click", ()=> {
+        purchaseUpgrade(upgrade);
+    })
+
+item.append(button);
+stall.appendChild(item);
+
+
+   });
+}
+
+fetchData()
+
+
+function purchaseUpgrade(upgrade) {
+    if (state.takoyakiCount >= upgrade.cost) {
+        state.takoyakiCount = sum(state.takoyakiCount, -upgrade.cost); 
+        state.tps = sum(state.tps, upgrade.increase); 
+        updateDisplay();                     
+    } else 
+        {
+        alert("Ah ah ah! You don't have enough tako to yaki!");
+    }
+}
